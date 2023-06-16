506af7cfbc4861d856a77d5b0bc27ca4d5627d6c
Katie Barnett <katie.barnett@bilue.com.au>
2023-06-16 19:22:34 +1000

    Upgrade of AGP to 8.2.0-alpha07

--- graphql/build.gradle.kts
+++ graphql/build.gradle.kts
@@ -21,9 +21,10 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    namespace = "dev.katiebarnett.experiments.graphql"
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
