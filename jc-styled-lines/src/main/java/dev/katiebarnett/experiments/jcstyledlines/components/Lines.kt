7ab1fcfe9275b9e52ed64b8c66b60579e6b386ec
Katie Barnett <katiebarnett5@gmail.com>
2024-03-17 17:59:22 +1100

    Lines work

--- jc-styled-lines/src/main/java/dev/katiebarnett/experiments/jcstyledlines/components/Lines.kt
+++ jc-styled-lines/src/main/java/dev/katiebarnett/experiments/jcstyledlines/components/Lines.kt
@@ -3,7 +3,6 @@ package dev.katiebarnett.experiments.jcstyledlines.components
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -49,24 +48,36 @@ fun SolidLineRoundedEnds(color: Color, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DashedLine(color: Color, modifier: Modifier = Modifier) {
+fun SolidLineSquareEnds(color: Color, modifier: Modifier = Modifier) {
+    Canvas(modifier) {
+        drawLine(
+            color = color,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            cap = StrokeCap.Square,
+            strokeWidth = LINE_WIDTH.toPx()
+        )
+    }
+}
+
+@Composable
+fun DashedLine(color: Color, phaseDivider: Int, modifier: Modifier = Modifier) {
     val density = LocalDensity.current
     with(density) {
         val dashOnInterval = (LINE_WIDTH * 4).toPx()
         val dashOffInterval = (LINE_WIDTH * 4).toPx()
 
-        val pathEffect =
-            PathEffect.dashPathEffect(
-                floatArrayOf(dashOnInterval, dashOffInterval),
-                0f
-            )
+        val pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(dashOnInterval, dashOffInterval),
+            phase = if (phaseDivider == 0) 0f else dashOnInterval / phaseDivider
+        )
         Canvas(modifier) {
             drawLine(
                 color = color,
                 start = Offset(0f, 0f),
                 end = Offset(size.width, 0f),
                 strokeWidth = LINE_WIDTH.toPx(),
-                cap = StrokeCap.Round,
+                cap = StrokeCap.Butt,
                 pathEffect = pathEffect,
             )
         }
@@ -84,8 +95,8 @@ fun MultiDashedLine(color: Color, modifier: Modifier = Modifier) {
 
         val pathEffect =
             PathEffect.dashPathEffect(
-                floatArrayOf(dashOnInterval1, dashOffInterval1, dashOnInterval2, dashOffInterval2),
-                0f
+                intervals = floatArrayOf(dashOnInterval1, dashOffInterval1, dashOnInterval2, dashOffInterval2),
+                phase = 0f
             )
         Canvas(modifier) {
             drawLine(
@@ -150,13 +161,14 @@ fun DottedSeparatedLine(color: Color, modifier: Modifier = Modifier) {
     }
 }
 
+// Heart shape:
+// https://blog.canopas.com/how-to-apply-stroke-effects-to-text-in-jetpack-compose-b1c02c9907bd
 @Composable
 fun HeartLine(color: Color, modifier: Modifier = Modifier) {
     val shapeRadius = LINE_WIDTH / 2
     val dotSpacing = shapeRadius * 4
     val density = LocalDensity.current
 
-    // https://blog.canopas.com/how-to-apply-stroke-effects-to-text-in-jetpack-compose-b1c02c9907bd
     val heartPath = remember {
         with(density) {
             Path().apply {
@@ -188,158 +200,122 @@ fun HeartLine(color: Color, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DoubleSolidDashedLine(color: Color, modifier: Modifier = Modifier) {
-    val dotRadius = LINE_WIDTH / 2
-    val dotSpacing = dotRadius * 4
-
+fun ZigZagLineTriangles(color: Color, modifier: Modifier = Modifier) {
+    val shapeWidth = LINE_WIDTH
     val density = LocalDensity.current
-    with(density) {
-        val dashOnInterval1 = (LINE_WIDTH * 4).toPx()
-        val dashOffInterval1 = (LINE_WIDTH * 2).toPx()
-        val dashOnInterval2 = (LINE_WIDTH / 4).toPx()
-        val dashOffInterval2 = (LINE_WIDTH * 2).toPx()
-
-        val circle = Path()
-        circle.addOval(Rect(center = Offset.Zero, radius = dotRadius.toPx()))
-        val dotPathEffect = PathEffect.stampedPathEffect(
-            shape = circle,
-            advance = dotSpacing.toPx(),
+    val zigZagPath = remember {
+        with(density) {
+            Path().apply {
+                val zigZagWidth = shapeWidth.toPx()
+                val zigZagHeight = shapeWidth.toPx()
+                moveTo(0f, 0f)
+                lineTo(zigZagWidth / 2, zigZagHeight / 2)
+                lineTo(zigZagWidth, 0f)
+            }
+        }
+    }
+    Canvas(modifier) {
+        val pathEffect = PathEffect.stampedPathEffect(
+            shape = zigZagPath,
+            advance = shapeWidth.toPx(),
             phase = 0f,
             style = StampedPathEffectStyle.Translate
         )
-
-        val solidPathEffect = PathEffect.cornerPathEffect(10f)
-
-        val multiDashPathEffect =
-            PathEffect.dashPathEffect(
-                floatArrayOf(dashOnInterval1, dashOffInterval1),
-                0f
-            )
-        Canvas(modifier) {
-
-            val chainedPathEffect = PathEffect.chainPathEffect(inner = multiDashPathEffect, outer = solidPathEffect)
-            drawLine(
-                color = color,
-                start = Offset(0f, 0f),
-                end = Offset(size.width, 0f),
-                pathEffect = chainedPathEffect,
-                strokeWidth = LINE_WIDTH.toPx()
-            )
-        }
-    }
-}
-
-@Composable
-fun WavyLine(color: Color, modifier: Modifier = Modifier) {
-val amplitude = 100f
-    Canvas(modifier = modifier.fillMaxSize()) {
-        val path = Path()
-        val centerY = size.height / 2f
-        val centerX = size.width / 2f
-
-        // Move to starting point (one-quarter width from left, center of canvas)
-        path.moveTo(centerX / 4f, centerY)
-
-        // Loop through half width of canvas, calculating y position for sine wave
-        for (x in 0f..centerX) {
-            val y = centerY + amplitude * Math.sin(2 * Math.PI * x / centerX)
-            path.lineTo(x, y)
-        }
-
-        // Since it's a single cycle, mirror the drawn path to complete the wave
-        path.lineTo(centerX, centerY) // Connect endpoint to center for mirroring
-        for (x in centerX downTo 0f) {
-            val mirroredY = centerY - amplitude * Math.sin(2 * Math.PI * (centerX - x) / centerX)
-            path.lineTo(x, mirroredY)
-        }
-
-        // Draw the completed path with blue color, stroke width, and round caps
-        drawPath(
-            path = path,
-            color = Color.Blue,
-            strokeWidth = 2.dp.toPx(),
-            strokeCap = StrokeCap.Round,
-            strokeJoin = StrokeJoin.Round
+        drawLine(
+            color = color,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect,
+            strokeWidth = shapeWidth.toPx()
         )
     }
-
-//    val density = LocalDensity.current
-//    val wavePath = remember {
-//        with(density) {
-//
-//            val height = LINE_WIDTH.toPx()
-//            val width = (LINE_WIDTH*4).toPx()
-//
-//            val wavePath = Path()
-//            val centerY = height / 2f
-//            val centerX = width / 2f
-//            val amplitude = 100f
-//
-//            // Move to starting point (one-quarter width from left, center of canvas)
-//            wavePath.moveTo(centerX / 4f, centerY)
-//
-//            // Loop through half width of canvas, calculating y position for sine wave
-//            for (x in 0f..centerX) {
-//                val y = centerY + amplitude * Math.sin(2 * Math.PI * x / centerX)
-//                wavePath.lineTo(x, y)
-//            }
-//
-//            // Since it's a single cycle, mirror the drawn path to complete the wave
-//            wavePath.lineTo(centerX, centerY) // Connect endpoint to center for mirroring
-//            for (x in centerX downTo 0f) {
-//                val mirroredY = centerY - amplitude * Math.sin(2 * Math.PI * (centerX - x) / centerX)
-//                wavePath.lineTo(x, mirroredY)
-//            }
-
-
-
-//
-//            Path().apply {
-//
-//
-//
-//                val path = Path()
-//                val centerY = (LINE_WIDTH / 2f).toPx()
-//
-//                // Move to starting point (left edge, center of canvas)
-//                path.moveTo(0f, centerY)
-//
-//                // Loop through width of canvas, calculating y position for sine wave
-//                for (x in 0f..size.width) {
-//                    val y = centerY + amplitude * Math.sin(x * Math.PI * frequency / size.width)
-//                    path.lineTo(x, y)
-//                }
-//
-//
-////                val width = (LINE_WIDTH * 4).toPx()
-////                val height = (LINE_WIDTH).toPx()
-////                moveTo(width / 2, height / 2)
-////                cubicTo(width / 4, 0f, 0f, height / 3, width / 4, height / 2)
-////                lineTo(width / 2, height * 3 / 4)
-////                lineTo(width * 3 / 4, height / 2)
-////                cubicTo(width, height / 3, width * 3 / 4, 0f, width / 2, height / 4)
-//            }
-//        }
-//    }
-//    Canvas(modifier) {
-//        val pathEffect = PathEffect.stampedPathEffect(
-//            shape = wavePath,
-//            advance = 0f,
-//            phase = 0f,
-//            style = StampedPathEffectStyle.Translate
-//        )
-//        drawLine(
-//            color = color,
-//            start = Offset(0f, 0f),
-//            end = Offset(size.width, 0f),
-//            pathEffect = pathEffect,
-//            strokeWidth = LINE_WIDTH.toPx()
-//        )
-//    }
 }
 
 @Composable
-fun GradientLine(color: Color, modifier: Modifier = Modifier) {
+fun ZigZagLineBaseline(color: Color, modifier: Modifier = Modifier) {
+    val shapeWidth = LINE_WIDTH
+    val density = LocalDensity.current
+    val zigZagPath = remember {
+        with(density) {
+            Path().apply {
+                val zigZagWidth = shapeWidth.toPx()
+                val zigZagHeight = shapeWidth.toPx()
+                val zigZagLineWidth = (1.dp).toPx()
+                moveTo(0f, 0f)
+                lineTo(zigZagWidth / 2, zigZagHeight / 2)
+                lineTo(zigZagWidth, 0f)
+                lineTo(zigZagWidth, 0f + zigZagLineWidth)
+                lineTo(zigZagWidth / 2, zigZagHeight / 2 + zigZagLineWidth)
+                lineTo(0f, 0f + zigZagLineWidth)
+            }
+        }
+    }
+    Canvas(modifier) {
+        val pathEffect = PathEffect.stampedPathEffect(
+            shape = zigZagPath,
+            advance = shapeWidth.toPx(),
+            phase = 0f,
+            style = StampedPathEffectStyle.Translate
+        )
+        drawLine(
+            color = Color.Magenta,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            strokeWidth = (1.dp).toPx()
+        )
+        drawLine(
+            color = color,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect,
+            strokeWidth = shapeWidth.toPx()
+        )
+    }
+}
+
+@Composable
+fun ZigZagLine(color: Color, modifier: Modifier = Modifier) {
+    val shapeWidth = LINE_WIDTH
+    val density = LocalDensity.current
+    val zigZagPath = remember {
+        with(density) {
+            Path().apply {
+                val zigZagWidth = shapeWidth.toPx()
+                val zigZagHeight = shapeWidth.toPx()
+                val zigZagLineWidth = (1.dp).toPx()
+                val shapeVerticalOffset = (zigZagHeight / 2) / 2
+                val shapeHorizontalOffset = (zigZagHeight / 2) / 2
+                moveTo(0f, 0f)
+                lineTo(zigZagWidth / 2, zigZagHeight / 2)
+                lineTo(zigZagWidth, 0f)
+                lineTo(zigZagWidth, 0f + zigZagLineWidth)
+                lineTo(zigZagWidth / 2, zigZagHeight / 2 + zigZagLineWidth)
+                lineTo(0f, 0f + zigZagLineWidth)
+                translate(Offset(-shapeHorizontalOffset, -shapeVerticalOffset))
+            }
+        }
+    }
+    Canvas(modifier) {
+        val pathEffect = PathEffect.stampedPathEffect(
+            shape = zigZagPath,
+            advance = shapeWidth.toPx(),
+            phase = 0f,
+            style = StampedPathEffectStyle.Translate
+        )
+//        drawLine(
+//            color = Color.Magenta,
+//            start = Offset(0f, 0f),
+//            end = Offset(size.width, 0f),
+//            strokeWidth = (1.dp).toPx()
+//        )
+        drawLine(
+            color = color,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect,
+            strokeWidth = shapeWidth.toPx()
+        )
+    }
 }
 
 @Preview(showBackground = true)
@@ -352,14 +328,18 @@ fun LinePreview() {
         ) {
             SolidLine(Color.Black, Modifier.fillMaxWidth())
             SolidLineRoundedEnds(Color.Red, Modifier.fillMaxWidth())
-            DashedLine(Color.Yellow, Modifier.fillMaxWidth())
+            SolidLineSquareEnds(Color.Blue, Modifier.fillMaxWidth())
+            DashedLine(Color.Yellow, 0, Modifier.fillMaxWidth())
+//            DashedLine(Color.Yellow, 2, Modifier.fillMaxWidth())
+//            DashedLine(Color.Yellow, 1, Modifier.fillMaxWidth())
             MultiDashedLine(Color.LightGray, Modifier.fillMaxWidth())
             DottedLine(Color.Green, Modifier.fillMaxWidth())
             DottedSeparatedLine(Color.Cyan, Modifier.fillMaxWidth())
             HeartLine(Color.Magenta, Modifier.fillMaxWidth())
-            DoubleSolidDashedLine(Color.Magenta, Modifier.fillMaxWidth())
-            WavyLine(Color.Blue, Modifier.fillMaxWidth())
-            GradientLine(Color.Blue, Modifier.fillMaxWidth())
+
+//            ZigZagLineTriangles(Color.Blue, Modifier.fillMaxWidth())
+//            ZigZagLineBaseline(Color.Blue, Modifier.fillMaxWidth())
+            ZigZagLine(Color.Blue, Modifier.fillMaxWidth())
         }
     }
 }
--- jc-styled-lines/src/main/java/dev/katiebarnett/experiments/jcstyledlines/components/Rectangles.kt
+++ jc-styled-lines/src/main/java/dev/katiebarnett/experiments/jcstyledlines/components/Rectangles.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
@@ -21,11 +21,62 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import dev.katiebarnett.experiments.core.theme.ExperimentsTheme
 
 private val LINE_WIDTH = 8.dp
 private val RECT_RADIUS = 40f
+
+// https://blog.canopas.com/how-to-apply-stroke-effects-to-text-in-jetpack-compose-b1c02c9907bd
+fun getHeartPath(density: Density): Path {
+    val shapeRadius = LINE_WIDTH / 2
+    with(density) {
+        return Path().apply {
+            val width = (shapeRadius * 2).toPx()
+            val height = (shapeRadius * 2).toPx()
+            moveTo(width / 2, height / 4)
+            cubicTo(width / 4, 0f, 0f, height / 3, width / 4, height / 2)
+            lineTo(width / 2, height * 3 / 4)
+            lineTo(width * 3 / 4, height / 2)
+            cubicTo(width, height / 3, width * 3 / 4, 0f, width / 2, height / 4)
+        }
+    }
+}
+
+fun getZigZagPath(density: Density): Path {
+    val shapeWidth = LINE_WIDTH
+    with(density) {
+        return Path().apply {
+            val zigZagWidth = shapeWidth.toPx()
+            val zigZagHeight = shapeWidth.toPx()
+            val zigZagLineWidth = (1.dp).toPx()
+            val shapeVerticalOffset = (zigZagHeight / 2) / 2
+            val shapeHorizontalOffset = (zigZagHeight / 2) / 2
+            moveTo(0f, 0f)
+            lineTo(zigZagWidth / 2, zigZagHeight / 2)
+            lineTo(zigZagWidth, 0f)
+            lineTo(zigZagWidth, 0f + zigZagLineWidth)
+            lineTo(zigZagWidth / 2, zigZagHeight / 2 + zigZagLineWidth)
+            lineTo(0f, 0f + zigZagLineWidth)
+            translate(Offset(-shapeHorizontalOffset, -shapeVerticalOffset))
+        }
+    }
+}
+
+fun getCubicPath(size: Size): Path {
+    return Path().apply {
+        moveTo(0f, 0f)
+        cubicTo(
+            x1 = size.width / 4,
+            y1 = size.height,
+            x2 = size.width / 4 * 3,
+            y2 = size.height / 4,
+            x3 = size.width,
+            y3 = size.height
+        )
+    }
+}
 
 @Composable
 fun MultiDashedRect(color: Color, modifier: Modifier = Modifier) {
@@ -56,25 +107,67 @@ fun MultiDashedRect(color: Color, modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun MultiDashedCircle(color: Color, modifier: Modifier = Modifier) {
+    val density = LocalDensity.current
+    with(density) {
+        val dashOnInterval1 = (LINE_WIDTH * 4).toPx()
+        val dashOffInterval1 = (LINE_WIDTH * 2).toPx()
+        val dashOnInterval2 = (LINE_WIDTH / 4).toPx()
+        val dashOffInterval2 = (LINE_WIDTH * 2).toPx()
+
+        val pathEffect =
+            PathEffect.dashPathEffect(
+                floatArrayOf(dashOnInterval1, dashOffInterval1, dashOnInterval2, dashOffInterval2),
+                0f
+            )
+        Canvas(modifier) {
+            drawCircle(
+                color = color,
+                style = Stroke(
+                    width = LINE_WIDTH.toPx(),
+                    cap = StrokeCap.Round,
+                    pathEffect = pathEffect,
+                ),
+            )
+        }
+    }
+}
+
+@Composable
+fun MultiDashedPath(color: Color, modifier: Modifier = Modifier) {
+    val density = LocalDensity.current
+    with(density) {
+        val dashOnInterval1 = (LINE_WIDTH * 4).toPx()
+        val dashOffInterval1 = (LINE_WIDTH * 2).toPx()
+        val dashOnInterval2 = (LINE_WIDTH / 4).toPx()
+        val dashOffInterval2 = (LINE_WIDTH * 2).toPx()
+
+        val pathEffect =
+            PathEffect.dashPathEffect(
+                floatArrayOf(dashOnInterval1, dashOffInterval1, dashOnInterval2, dashOffInterval2),
+                0f
+            )
+        Canvas(modifier) {
+            val path = getCubicPath(size)
+            drawPath(
+                color = Color.LightGray,
+                path = path,
+                style = Stroke(
+                    width = LINE_WIDTH.toPx(),
+                    cap = StrokeCap.Round,
+                    pathEffect = pathEffect
+                )
+            )
+        }
+    }
+}
+
+@Composable
 fun HeartRectTranslate(color: Color, modifier: Modifier = Modifier) {
     val shapeRadius = LINE_WIDTH / 2f
     val dotSpacing = shapeRadius * 4
     val density = LocalDensity.current
-
-    // https://blog.canopas.com/how-to-apply-stroke-effects-to-text-in-jetpack-compose-b1c02c9907bd
-    val heartPath = remember {
-        with(density) {
-            Path().apply {
-                val width = (shapeRadius * 2).toPx()
-                val height = (shapeRadius * 2).toPx()
-                moveTo(width / 2, height / 4)
-                cubicTo(width / 4, 0f, 0f, height / 3, width / 4, height / 2)
-                lineTo(width / 2, height * 3 / 4)
-                lineTo(width * 3 / 4, height / 2)
-                cubicTo(width, height / 3, width * 3 / 4, 0f, width / 2, height / 4)
-            }
-        }
-    }
+    val heartPath = remember { getHeartPath(density) }
     Canvas(modifier) {
         val pathEffect = PathEffect.stampedPathEffect(
             shape = heartPath,
@@ -98,21 +191,7 @@ fun HeartRectRotate(color: Color, modifier: Modifier = Modifier) {
     val shapeRadius = LINE_WIDTH / 2f
     val dotSpacing = shapeRadius * 4
     val density = LocalDensity.current
-
-    // https://blog.canopas.com/how-to-apply-stroke-effects-to-text-in-jetpack-compose-b1c02c9907bd
-    val heartPath = remember {
-        with(density) {
-            Path().apply {
-                val width = (shapeRadius * 2).toPx()
-                val height = (shapeRadius * 2).toPx()
-                moveTo(width / 2, height / 4)
-                cubicTo(width / 4, 0f, 0f, height / 3, width / 4, height / 2)
-                lineTo(width / 2, height * 3 / 4)
-                lineTo(width * 3 / 4, height / 2)
-                cubicTo(width, height / 3, width * 3 / 4, 0f, width / 2, height / 4)
-            }
-        }
-    }
+    val heartPath = remember { getHeartPath(density) }
     Canvas(modifier) {
         val pathEffect = PathEffect.stampedPathEffect(
             shape = heartPath,
@@ -136,21 +215,7 @@ fun HeartRectMorph(color: Color, modifier: Modifier = Modifier) {
     val shapeRadius = LINE_WIDTH / 2f
     val dotSpacing = shapeRadius * 4
     val density = LocalDensity.current
-
-    // https://blog.canopas.com/how-to-apply-stroke-effects-to-text-in-jetpack-compose-b1c02c9907bd
-    val heartPath = remember {
-        with(density) {
-            Path().apply {
-                val width = (shapeRadius * 2).toPx()
-                val height = (shapeRadius * 2).toPx()
-                moveTo(width / 2, height / 4)
-                cubicTo(width / 4, 0f, 0f, height / 3, width / 4, height / 2)
-                lineTo(width / 2, height * 3 / 4)
-                lineTo(width * 3 / 4, height / 2)
-                cubicTo(width, height / 3, width * 3 / 4, 0f, width / 2, height / 4)
-            }
-        }
-    }
+    val heartPath = remember { getHeartPath(density) }
     Canvas(modifier) {
         val pathEffect = PathEffect.stampedPathEffect(
             shape = heartPath,
@@ -170,49 +235,156 @@ fun HeartRectMorph(color: Color, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DoubleSolidDashedRect(color: Color, modifier: Modifier = Modifier) {
-    val dotRadius = 8.dp
-    val dotSpacing = dotRadius * 4
-
+fun ZigZagRectTranslate(color: Color, modifier: Modifier = Modifier) {
+    val shapeWidth = LINE_WIDTH
+    val density = LocalDensity.current
+    val zigZagPath = remember { getZigZagPath(density) }
     Canvas(modifier) {
-        val circle = Path()
-        circle.addOval(Rect(center = Offset.Zero, radius = dotRadius.toPx()))
-        val solidPathEffect = PathEffect.dashPathEffect(
-            floatArrayOf(10f, 20f),
-            25f
-        )
-        val dotPathEffect = PathEffect.stampedPathEffect(
-            shape = circle,
-            advance = dotSpacing.toPx(),
+        val pathEffect = PathEffect.stampedPathEffect(
+            shape = zigZagPath,
+            advance = shapeWidth.toPx(),
             phase = 0f,
             style = StampedPathEffectStyle.Translate
         )
-
-        val chainedPathEffect = PathEffect.chainPathEffect(inner = solidPathEffect, outer = dotPathEffect)
         drawRoundRect(
             color = color,
             cornerRadius = CornerRadius(RECT_RADIUS),
             style = Stroke(
                 width = LINE_WIDTH.toPx(),
-                pathEffect = chainedPathEffect,
+                pathEffect = pathEffect,
             ),
         )
     }
 }
 
+@Composable
+fun ZigZagRectRotate(color: Color, modifier: Modifier = Modifier) {
+    val shapeWidth = LINE_WIDTH
+    val density = LocalDensity.current
+    val zigZagPath = remember { getZigZagPath(density) }
+    Canvas(modifier) {
+        val pathEffect = PathEffect.stampedPathEffect(
+            shape = zigZagPath,
+            advance = shapeWidth.toPx(),
+            phase = 0f,
+            style = StampedPathEffectStyle.Rotate
+        )
+        drawRoundRect(
+            color = color,
+            cornerRadius = CornerRadius(RECT_RADIUS),
+            style = Stroke(
+                width = LINE_WIDTH.toPx(),
+                pathEffect = pathEffect,
+            ),
+        )
+    }
+}
+
+@Composable
+fun ZigZagRectMorph(color: Color, modifier: Modifier = Modifier) {
+    val shapeWidth = LINE_WIDTH
+    val density = LocalDensity.current
+    val zigZagPath = remember { getZigZagPath(density) }
+    Canvas(modifier) {
+        val pathEffect = PathEffect.stampedPathEffect(
+            shape = zigZagPath,
+            advance = shapeWidth.toPx(),
+            phase = 0f,
+            style = StampedPathEffectStyle.Morph
+        )
+        drawRoundRect(
+            color = color,
+            cornerRadius = CornerRadius(RECT_RADIUS),
+            style = Stroke(
+                width = LINE_WIDTH.toPx(),
+                pathEffect = pathEffect,
+            ),
+        )
+    }
+}
+
+// ????
+// @Composable
+// fun DoubleSolidDashedRect(color: Color, modifier: Modifier = Modifier) {
+//    val dotRadius = 8.dp
+//    val dotSpacing = dotRadius * 4
+//
+//    Canvas(modifier) {
+//        val circle = Path()
+//        circle.addOval(Rect(center = Offset.Zero, radius = dotRadius.toPx()))
+//        val solidPathEffect = PathEffect.dashPathEffect(
+//            floatArrayOf(10f, 20f),
+//            25f
+//        )
+//        val dotPathEffect = PathEffect.stampedPathEffect(
+//            shape = circle,
+//            advance = dotSpacing.toPx(),
+//            phase = 0f,
+//            style = StampedPathEffectStyle.Translate
+//        )
+//
+//        val chainedPathEffect = PathEffect.chainPathEffect(inner = solidPathEffect, outer = dotPathEffect)
+//        drawRoundRect(
+//            color = color,
+//            cornerRadius = CornerRadius(RECT_RADIUS),
+//            style = Stroke(
+//                width = LINE_WIDTH.toPx(),
+//                pathEffect = chainedPathEffect,
+//            ),
+//        )
+//    }
+// }
+
 @Preview(showBackground = true)
 @Composable
-fun RectanglesPreview() {
+fun ShapesPreview() {
     ExperimentsTheme {
         Row(
             horizontalArrangement = Arrangement.spacedBy(LINE_WIDTH * 2),
-            modifier = Modifier.padding(16.dp).fillMaxWidth().height(100.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(100.dp)
         ) {
             MultiDashedRect(Color.LightGray, Modifier.fillMaxHeight().weight(1f))
+            MultiDashedCircle(Color.LightGray, Modifier.fillMaxHeight().weight(1f))
+            MultiDashedPath(Color.LightGray, Modifier.fillMaxHeight().weight(1f))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeartShapesPreview() {
+    ExperimentsTheme {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(LINE_WIDTH * 2),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(100.dp)
+        ) {
             HeartRectTranslate(Color.Magenta, Modifier.fillMaxHeight().weight(1f))
             HeartRectRotate(Color.Magenta, Modifier.fillMaxHeight().weight(1f))
             HeartRectMorph(Color.Magenta, Modifier.fillMaxHeight().weight(1f))
-            DoubleSolidDashedRect(Color.Magenta, Modifier.fillMaxHeight().weight(1f))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaveShapesPreview() {
+    ExperimentsTheme {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(LINE_WIDTH * 2),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(100.dp)
+        ) {
+            ZigZagRectTranslate(Color.Blue, Modifier.fillMaxHeight().weight(1f))
+            ZigZagRectRotate(Color.Blue, Modifier.fillMaxHeight().weight(1f))
+            ZigZagRectMorph(Color.Blue, Modifier.fillMaxHeight().weight(1f))
         }
     }
 }
